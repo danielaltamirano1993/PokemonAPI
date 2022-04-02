6d82fb7b1201324a7e1a57f8c88c98800454b278
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2022-04-02 18:38:05 -0500

    Create note.js

---
+++ note.js
@@ -0,0 +1,26 @@
+function scss() {
+  estilos.css;
+}
+
+function css_tricks() {
+  box - sizing;
+}
+
+function flex() {}
+
+//SASS es CSS compilado para celulares y todo tipo de dispositivos
+//Instalar Live Sass Compiler
+//crear un archivo por ejemplo estilos.scss
+//en dentro del archivo ejecutar Watch Sass
+//Se crea un archivo estilos.css
+//Link este archivo y listo
+
+//css_tricks
+//documentacion de estilos y elementos del DOM
+
+//flex
+//Sirve para ubicar objetos en un arbol con estruturas de hijosDOM
+
+//Extensiones en Chrome o Brave
+//ColorPick Eyedropper
+//Page Ruler
